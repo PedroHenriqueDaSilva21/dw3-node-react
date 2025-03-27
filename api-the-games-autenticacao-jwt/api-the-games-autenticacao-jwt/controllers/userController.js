778ae3cb379b6c043fba44add39c89cdfc6a7e7d
PedroHenriqueDaSilva21 <pedro.henriquedasilva00001@gmail.com>
2025-03-26 22:25:08 -0300

    :sparkles:feat: implementação de JWT nas rotas

--- api-the-games-autenticacao-jwt/api-the-games-autenticacao-jwt/controllers/userController.js
+++ api-the-games-autenticacao-jwt/api-the-games-autenticacao-jwt/controllers/userController.js
@@ -57,4 +57,4 @@ const loginUser = async (req, res) => {
   }
 };
 
-export default { createUser, loginUser };
+export default { createUser, loginUser,JWTSecret };
--- api-the-games-autenticacao-jwt/api-the-games-autenticacao-jwt/routes/userRoutes.js
+++ api-the-games-autenticacao-jwt/api-the-games-autenticacao-jwt/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import express from "express"
 import userController from "../controllers/userController.js"
+import Auth from "../middleware/Auth.js";
 const userRoutes = express.Router()
 
 userRoutes.post("/user",userController.createUser);
