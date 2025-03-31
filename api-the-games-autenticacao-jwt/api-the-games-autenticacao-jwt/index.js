e77beb86af35ad9d7a5424d5ab5330ba89107be4
Pedro Henrique da Silva <pedro.henriquedasilva00001@gmail.com>
2025-03-31 13:59:56 -0300

    :wrench:ci/cd: troca de banco para MongoDB Atlas

--- api-the-games-autenticacao-jwt/api-the-games-autenticacao-jwt/index.js
+++ api-the-games-autenticacao-jwt/api-the-games-autenticacao-jwt/index.js
@@ -2,7 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import Games from "./models/Games.js"
 import User from "./models/Users.js";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
+
 
 // Importando as rotas (endpoints) de Games
 import gameRoutes from './routes/gameRoutes.js'
@@ -15,7 +19,13 @@ app.use('/', gameRoutes)
 app.use('/',userRoutes)
 
 // Iniciando a conexão com o banco de dados do MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/api-thegames")
+//mongoose.connect("mongodb://127.0.0.1:27017/api-thegames")
+
+//Variáveis de Ambiente para acesso ao MongoDB Atlas
+const user = process.env.user
+const password = process.env.password
+
+mongoose.connect(`mongodb+srv://${process.env.password}:${process.env.password}@clustermotorteste.fmlpihp.mongodb.net/?retryWrites=true&w=majority&appName=ClusterMotorTeste`)
 
 // Iniciando o servidor
 const port = 4000;
